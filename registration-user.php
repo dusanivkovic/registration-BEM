e3ca2ca308a4c112b760f86dba8af57281ffdc4c
https://github.com/dusanivkovic <ivdule77@gmail.com>
2023-12-05 11:51:51 +0100

    Adding validation for email input

--- registration-user.php
+++ registration-user.php
@@ -9,30 +9,9 @@ const EMAIL_REQUIRED = 'Please enter your email';
 const EMAIL_INVALID = 'Please enter a valid email';
 const PASSWORD_CONFIRM = 'Your password must match';
 $errors = [];
-
-
-if (isset($_POST['submit'])) {
-    $user_name = $_POST['user-name'];
-    $user_mail = $_POST['user-mail'];
-    $user_password = $_POST['user-password'];
-    $repeat_password = $_POST['password-again'];
-    $date_birth = "{$_POST['year-birth']}-{$_POST['month-birth']}-{$_POST['day-birth']}";
-    $user_registration = "INSERT INTO user_account VALUES ('', '$user_name', '$user_mail', '$user_password', '$date_birth')";
-    if ($user_password != $repeat_password)
-    {
-        $message = 'Your password does not match! Try again!';
-        header('location: registration-user.php?error=confirm');
-    }elseif ($conn -> query($user_registration))
-    {
-        echo 'Insert data successfuly!';
-        header('location: index.php');
-    }else
-    {
-        echo 'Error inserting data! '. $user_registration . $conn -> connect_error;
-    }
-}else
+$request_method = strtoupper($_SERVER['REQUEST_METHOD']);
+if ($request_method == 'GET')
 {
-
     if (isset($_GET['options'])) 
     {
         $option = test_input($_GET['options']);
@@ -44,24 +23,68 @@ if (isset($_POST['submit'])) {
         {
             echo 'Error 404. File not found!';
         }
-    }else 
+    }else
     {
         if ($_GET['error'] == 'confirm')
         {
             $errors['confirm'] = PASSWORD_CONFIRM;
             echo $errors['confirm'].' Try again! <a href="registration-user.php?options=registration">...</a>';
         }
+        if ($_GET['error'] == 'email_invalid')
+        {
+            $errors['email'] = EMAIL_INVALID;
+            echo $errors['email'];
+        }
+        if ($_GET['error'] == 'email_required')
+        {
+            $errors['email'] = EMAIL_REQUIRED;
+            echo $errors['email'];
+        }
     }
-}
-if (isset($_POST['login']))
+}elseif ($request_method == 'POST')
 {
     $user_mail = filter_input(INPUT_POST, 'user-mail', FILTER_SANITIZE_EMAIL);
-    $user_password = filter_input(INPUT_POST, 'user-password', FILTER_SANITIZE_ENCODED);
-    echo $user_password;
+    $user_name = $_POST['user-name'];
+    // $user_mail = $_POST['user-mail'];
+    $user_password = $_POST['user-password'];
+    $repeat_password = $_POST['password-again'];
+    $date_birth = "{$_POST['year-birth']}-{$_POST['month-birth']}-{$_POST['day-birth']}";
+    if ($user_mail) {
+        // validate email
+        $user_mail = filter_var($user_mail, FILTER_VALIDATE_EMAIL);
+        if ($user_mail === false) {
+            header('location: registration-user.php?error=email_invalid');
+            $errors['email'] = '';
+        }
+    } else {
+        header('location: registration-user.php?error=email_required');
+        $errors['email'] = '';
+    }
+    if ($user_password != $repeat_password)
+    {
+        header('location: registration-user.php?error=confirm');
+    }
+    if (count($errors) == 0)
+    {    
+        if (isset($_POST['submit'])) {
 
+            $user_registration = "INSERT INTO user_account VALUES ('', '$user_name', '$user_mail', '$user_password', '$date_birth')";
+            if ($conn -> query($user_registration))
+            {
+                echo 'Insert data successfuly!';
+                header('location: index.php');
+            }else
+            {
+                echo 'Error inserting data! '. $user_registration . $conn -> connect_error;
+            }
+        }elseif (isset($_POST['login']))
+        {
+            $user_password = filter_input(INPUT_POST, 'user-password', FILTER_SANITIZE_ENCODED);
+            echo $user_password;
+        }
+    }
 }
-?>
-<?php
+
 $conn -> close();
 include 'partials/footer.php'; ?>
 
